b43686eba5b6e8bb2be5a017ffee7f5a46cf02c8
Lutfhi dedevalen <lutvie72@gmail.com>
2018-05-22 21:57:02 +0700

    navigation fix

--- app/src/main/java/skripsi/dita/antrianklinik/BottomNavMenu.java
+++ app/src/main/java/skripsi/dita/antrianklinik/BottomNavMenu.java
@@ -46,7 +46,36 @@ public class BottomNavMenu extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
 
+    }
+
+    /**
+     *0 Navigation Helper
+     */
+    private static class BottomNavigationViewHelper {
+        @SuppressLint("RestrictedApi")
+        static void disableShiftMode(BottomNavigationView view) {
+            BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+            try {
+                Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+                shiftingMode.setAccessible(true);
+                shiftingMode.setBoolean(menuView, false);
+                shiftingMode.setAccessible(false);
+                for (int i = 0; i < menuView.getChildCount(); i++) {
+                    BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                    //noinspection RestrictedApi
+                    item.setShiftingMode(false);
+                    // set once again checked value, so view will be updated
+                    //noinspection RestrictedApi
+                    item.setChecked(item.getItemData().isChecked());
+                }
+            } catch (NoSuchFieldException e) {
+                Log.e("BottomNavigation : ", "Unable to get shift mode field", e);
+            } catch (IllegalAccessException e) {
+                Log.e("BottomNavigation : ", "Unable to change value of shift mode", e);
+            }
+        }
     }
 
 }
