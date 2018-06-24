3f805318876ccaec872a8450eead956af961c638
k.adi@greenlabs.or.id <lutvie72@gmail.com>
2018-06-24 20:31:47 +0700

    fix

--- app/src/main/java/skripsi/dita/antrianklinik/KlinikListAdapter.java
+++ app/src/main/java/skripsi/dita/antrianklinik/KlinikListAdapter.java
@@ -1,8 +1,7 @@
 package skripsi.dita.antrianklinik;
 
-import android.app.Activity;
 import android.content.Context;
-import android.view.LayoutInflater;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -16,46 +15,58 @@ import skripsi.dita.antrianklinik.model.Klinik;
  * Created by Dita on 24/06/2018.
  */
 
-public class KlinikListAdapter extends ArrayAdapter{
+public class KlinikListAdapter extends ArrayAdapter<Klinik> {
+    // Your sent context
     private Context context;
-    private boolean useList = true;
+    // Your custom values for the spinner (User)
+    private List<Klinik> values;
 
-    public KlinikListAdapter (Context context, List items){
-        super(context, android.R.layout.simple_list_item_1, items);
+    public KlinikListAdapter(Context context, int textViewResourceId, List<Klinik> values) {
+        super(context, textViewResourceId, values);
         this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public int getCount() {
+        return values.size();
+    }
+
+    @Override
+    public Klinik getItem(int position) {
+        return values.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
 
-    private class ViewHolder{
-        TextView titleText;
-    }
-
-
+    // And the "magic" goes here
+    // This is for the "passive" state of the spinner
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder = null;
-        Klinik item = (Klinik) getItem(position);
-        View viewToUse = null;
+        // I created a dynamic TextView here, but you can reference your own  custom layout for each spinner item
+        TextView label = (TextView) super.getView(position, convertView, parent);
+        label.setTextColor(Color.BLACK);
+        // Then you can get the current item using the values array (Users array) and the current position
+        // You can NOW reference each method you has created in your bean object (User class)
+        label.setText(values.get(position).getNama_dokter());
 
-        // This block exists to inflate the settings list item conditionally based on whether
-        // we want to support a grid or list view.
-        LayoutInflater mInflater = (LayoutInflater) context
-                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        if (convertView == null) {
-            if(useList){
-                viewToUse = mInflater.inflate(R.layout.klinik_list_item, null);
-            } else {
-                viewToUse = mInflater.inflate(R.layout.klinik_grid_item, null);
-            }
+        // And finally return your dynamic (or custom) view for each spinner item
+        return label;
+    }
 
-            holder = new ViewHolder();
-            holder.titleText = (TextView)viewToUse.findViewById(R.id.titleTextView);
-            viewToUse.setTag(holder);
-        } else {
-            viewToUse = convertView;
-            holder = (ViewHolder) viewToUse.getTag();
-        }
+    // And here is when the "chooser" is popped up
+    // Normally is the same view, but you can customize it if you want
+    @Override
+    public View getDropDownView(int position, View convertView,
+                                ViewGroup parent) {
+        TextView label = (TextView) super.getDropDownView(position, convertView, parent);
+        label.setTextColor(Color.BLACK);
+        label.setText(values.get(position).getNama_dokter());
 
-        holder.titleText.setText(item.getRuang());
-        return viewToUse;
+        return label;
     }
 }
