9783bb2e433819b5985a10246808386a66a928c3
k.adi@greenlabs.or.id <lutvie72@gmail.com>
2018-07-14 21:41:27 +0700

    fix

--- app/src/main/java/skripsi/dita/antrianklinik/JadwalDokter.java
+++ app/src/main/java/skripsi/dita/antrianklinik/JadwalDokter.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -40,6 +43,23 @@ public class JadwalDokter extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
+
+        EditText editTextSearch = (EditText)findViewById(R.id.editText4);
+
+        editTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mAdapter.getFilter().filter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
         KlinikData();
     }
--- app/src/main/java/skripsi/dita/antrianklinik/KlinikAdapter.java
+++ app/src/main/java/skripsi/dita/antrianklinik/KlinikAdapter.java
@@ -6,8 +6,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import skripsi.dita.antrianklinik.model.Klinik;
@@ -16,26 +19,37 @@ import skripsi.dita.antrianklinik.model.Klinik;
  * Created by Dita on 13/05/2018.
  */
 
-public class KlinikAdapter extends RecyclerView.Adapter<KlinikAdapter.MyViewHolder>{
+public class KlinikAdapter extends RecyclerView.Adapter<KlinikAdapter.MyViewHolder> implements Filterable {
 
     private List<Klinik> klinikList;
+    private List<Klinik> filteredList;
+    private CustomFilter filter;
     private Context context;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView namaKlinik;
 
-        public MyViewHolder(View view){
+        public MyViewHolder(View view) {
             super(view);
             namaKlinik = (TextView) view.findViewById(R.id.txtKlinik);
         }
     }
 
-    public KlinikAdapter(List<Klinik> poliklinikList){
+    public KlinikAdapter(List<Klinik> poliklinikList) {
         this.klinikList = poliklinikList;
+        filteredList = new ArrayList<>();
+        filteredList.addAll(klinikList);
+        filter = new CustomFilter(this);
+    }
+
+    public KlinikAdapter(List<Klinik> poliklinikList, Context context) {
+        this.klinikList = poliklinikList;
+        this.context = context;
+
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         context = parent.getContext();
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.itemview_poliklinik, parent, false);
 
@@ -43,7 +57,7 @@ public class KlinikAdapter extends RecyclerView.Adapter<KlinikAdapter.MyViewHold
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, final int position){
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
         Klinik klinik = klinikList.get(position);
         holder.namaKlinik.setText(klinik.getRuang());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +74,51 @@ public class KlinikAdapter extends RecyclerView.Adapter<KlinikAdapter.MyViewHold
     }
 
     @Override
-    public int getItemCount(){
+    public Filter getFilter() {
+        return filter;
+    }
+
+    @Override
+    public int getItemCount() {
         return klinikList.size();
+    }
+
+    public class CustomFilter extends Filter {
+        private KlinikAdapter mAdapter;
+
+        private CustomFilter(KlinikAdapter mAdapter) {
+            super();
+            this.mAdapter = mAdapter;
+        }
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            System.out.println("filtered");
+            filteredList.clear();
+            final FilterResults results = new FilterResults();
+            if (constraint.length() == 0) {
+                filteredList.addAll(klinikList);
+                mAdapter.notifyDataSetChanged();
+            } else {
+                final String filterPattern = constraint.toString().toLowerCase().trim();
+                for (final Klinik klinik : klinikList) {
+                    if (klinik.getRuang().toLowerCase().startsWith(filterPattern)) {
+                        filteredList.add(klinik);
+                    }
+
+                    System.out.println("fill");
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            results.values = filteredList;
+            results.count = filteredList.size();
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            this.mAdapter.notifyDataSetChanged();
+        }
     }
 }
