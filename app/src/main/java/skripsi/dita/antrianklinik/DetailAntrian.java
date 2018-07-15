60b8643e25beae4bfe612885f7f0643bc08cd67a
Dita <pamuditas@gmail.com>
2018-07-15 22:13:21 +0700

    commit detail antrian

--- app/src/main/java/skripsi/dita/antrianklinik/DetailAntrian.java
+++ app/src/main/java/skripsi/dita/antrianklinik/DetailAntrian.java
@@ -3,14 +3,28 @@ package skripsi.dita.antrianklinik;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DetailAntrian extends AppCompatActivity {
+    TextView noAntrian, namaKlinik, noRm, namaPasien, jk, tglPeriksa, poli, dokter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_antrian);
         this.setTitle("Detail Antrian");
+
+        noAntrian   = (TextView) findViewById(R.id.txtNoAntrian);
+        namaKlinik  = (TextView) findViewById(R.id.txtNamaKlinik);
+        noRm        = (TextView) findViewById(R.id.txtNoRM);
+        namaPasien  = (TextView) findViewById(R.id.txtNamaPasien);
+        jk          = (TextView) findViewById(R.id.txtJK);
+        tglPeriksa  = (TextView) findViewById(R.id.txtTglperiksa);
+        poli        = (TextView) findViewById(R.id.txtKlinik);
+        dokter      = (TextView) findViewById(R.id.txtDokter);
+
     }
 
     @Override
